6f6eb1592b22c387ba0704d59cfdd73423ae4adb
Daniel Maia <danssmaia.dm@gmail.com>
2024-06-10 20:32:22 -0300

    Criação da rede Neural

--- ai.py
+++ ai.py
@@ -1,2 +1,43 @@
 # -*- coding: utf-8 -*-
 
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+# Criação da arquitetura da rede neural
+class Network(nn.Module):
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        
+        # 5 -> 30 -> 3 - full connection (dense)
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+        
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
+    
+# Implementação do replay de experiência
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        
+    # D,E,F,G,H
+    # 4 valores: último estado, novo estado, última ação, última recompensa
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+            
+    def sample(self, batch_size):
+        # ((1,2,3), (4,5,6)) -> ((1,4), (2,5), (3,6))
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
